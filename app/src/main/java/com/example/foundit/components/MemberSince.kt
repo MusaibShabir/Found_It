4c7b1a293e1a072eef88b3a116d23682cde763fa
Musaib Shabir <bnny633@gmail.com>
2024-07-01 19:19:03 +0530

    Function accepts the date as input argument

--- app/src/main/java/com/example/foundit/components/MemberSince.kt
+++ app/src/main/java/com/example/foundit/components/MemberSince.kt
@@ -1,38 +1,47 @@
 package com.example.foundit.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.foundit.R
 
 @Composable
-fun MemberSinceCard() {
+fun MemberSinceCard(
+    modifier: Modifier,
+    date: String //to be changed later to LocalDate
+) {
     Card(
         shape = RoundedCornerShape(15.dp),
-        modifier = Modifier
+        modifier = modifier
             .padding(16.dp)
             .fillMaxWidth(),
 
         ) {
         Column(
-            modifier = Modifier
+            modifier = modifier
                 .padding(16.dp)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.Start
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth()
+                modifier = modifier.fillMaxWidth()
             ) {
                 Text(
-                    text = "Found It Member Since",
+                    text = stringResource(id = R.string.member_card_heading),
                     fontWeight = FontWeight.Light,
                     fontSize = 20.sp,
                     modifier = Modifier.padding(bottom = 8.dp)
@@ -43,7 +52,7 @@ fun MemberSinceCard() {
                 horizontalArrangement = Arrangement.Start
             ) {
                 Text(
-                    text = "10 - June - 2024",
+                    text =  date, //"10 - June - 2024",
                     fontSize = 20.sp,
                     fontStyle = FontStyle.Italic
                 )
@@ -54,5 +63,8 @@ fun MemberSinceCard() {
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun PreviewMemberSince() {
-    MemberSinceCard()
+    MemberSinceCard(
+        modifier = Modifier,
+        date = "10 - June - 2024"
+    )
 }
