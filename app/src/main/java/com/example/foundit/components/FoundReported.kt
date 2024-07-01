3e4febfd66fb15e140f7d6f517dd2de78767e4cd
Musaib Shabir <bnny633@gmail.com>
2024-07-01 20:00:35 +0530

    Made the function more optimized

--- app/src/main/java/com/example/foundit/components/FoundReported.kt
+++ app/src/main/java/com/example/foundit/components/FoundReported.kt
@@ -1,6 +1,7 @@
 package com.example.foundit.components
 
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -10,52 +11,96 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.foundit.R
 
 @Composable
-fun FoundReportedCard() {
+fun FoundReportedCard(
+    modifier: Modifier
+) {
     Card(
         shape = RoundedCornerShape(15.dp),
-        modifier = Modifier
+        modifier = modifier
+            .fillMaxWidth()
             .padding(16.dp)
-            .fillMaxWidth(),
+            .height(100.dp),
     ) {
-        Column(
+        Box(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
+                .fillMaxSize()
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Found", fontSize = 20.sp,fontWeight = FontWeight.Light)
-                    Text(text = "4", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-                }
-
-                VerticalDivider(color= Color.White, modifier =
-                Modifier
+            VerticalDivider(
+                color = Color.Black,
+                modifier = Modifier
                     .width(2.dp)
-                    .height(70.dp))
+                    .height(70.dp)
+                    .align(Alignment.Center)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
 
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Reported", fontSize = 20.sp,fontWeight = FontWeight.Light)
-                    Text(text = "10", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-                }
+                ScoreSection(modifier = Modifier, sectionHeading = R.string.score_card_section_heading_left, sectionScore = 10)
+                ScoreSection(modifier = Modifier, sectionHeading = R.string.score_card_section_heading_right, sectionScore = 3)
             }
         }
+
     }
 }
+
+
+
+
+@Composable
+fun ScoreSection(
+    modifier: Modifier,
+    @StringRes
+    sectionHeading: Int,
+    sectionScore: Int
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ){
+        Text(
+            text = stringResource(id = sectionHeading),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Light
+        )
+        Spacer(modifier = modifier.height(5.dp))
+
+        Text(
+            text = sectionScore.toString(),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun PreviewFoundReported() {
-    FoundReportedCard()
+    FoundReportedCard(
+        modifier = Modifier
+    )
+}
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun PreviewScoreSection() {
+    ScoreSection(
+        modifier = Modifier,
+        sectionHeading = R.string.score_card_section_heading_left,
+        sectionScore = 10
+    )
 }
