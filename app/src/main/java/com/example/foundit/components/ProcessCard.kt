d660feab11db323c87f47fb8da0ac83ca8498c06
Musaib Shabir <bnny633@gmail.com>
2024-07-04 16:23:14 +0530

    Correct Colors Added

--- app/src/main/java/com/example/foundit/components/ProcessCard.kt
+++ app/src/main/java/com/example/foundit/components/ProcessCard.kt
@@ -2,6 +2,7 @@ package com.example.foundit.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
@@ -9,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.foundit.ui.theme.MainGreen
+import com.example.foundit.ui.theme.MainRed
 
 @Composable
 fun ProcessCard(
@@ -17,15 +20,16 @@ fun ProcessCard(
 ) {
     // Defining Card Color
     val cardColor = when (cardColorCode) {
-        0 -> Color.Red  // Later to be Changed
-        1 -> Color.Green // Later to be Changed
+        0 -> MainRed
+        1 -> MainGreen
         else -> Color.Gray
     }
     Card(
         onClick = { /*TODO*/ },
         modifier = modifier
             .fillMaxWidth()
-            .height(150.dp),
+            .height(150.dp)
+            .padding(16.dp),
         colors = CardDefaults.cardColors(containerColor = cardColor)
     ) {
 
