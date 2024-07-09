6d3218b512480267ee26f7431446a21a7d6a7b22
Musaib Shabir <bnny633@gmail.com>
2024-07-10 04:08:14 +0530

    Minor Modification

--- app/src/main/java/com/example/foundit/MainActivity.kt
+++ app/src/main/java/com/example/foundit/MainActivity.kt
@@ -7,11 +7,8 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.foundit.ui.theme.screens.HomeScreen
-import com.example.foundit.ui.theme.screens.MainScreen
+import com.example.foundit.presentation.screens.MainScreen
 import com.example.foundit.ui.theme.FoundItTheme
 
 
@@ -30,14 +27,5 @@ class MainActivity : ComponentActivity() {
                 MainScreen(modifier = Modifier)
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoundItTheme {
-        HomeScreen(modifier = Modifier)
     }
 }
