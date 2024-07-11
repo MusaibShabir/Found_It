acfe77ef1b981a38e65ef5e9e6272b2626fa588a
huzaif1234 <huzaifqazi25@gmail.com>
2024-07-12 01:50:28 +0530

    content added to ProgressCard -> title,LinearProgressIndicator,location,description

--- app/src/main/java/com/example/foundit/presentation/screens/process_screen/components/ProcessCard.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/process_screen/components/ProcessCard.kt
@@ -1,6 +1,7 @@
 package com.example.foundit.presentation.screens.process_screen.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,11 +11,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import com.example.foundit.data.FinishedProcessDataItem
 import com.example.foundit.data.InProcessDataItem
@@ -50,18 +53,38 @@ fun ProcessCard(
         onClick = { /*TODO*/ },
         modifier = modifier
             .fillMaxWidth()
-            .height(150.dp)
+            .height(160.dp)
             .padding(16.dp),
         colors = CardDefaults.cardColors(containerColor = cardColor)
     ) {
-        Row(
+        Column (
             modifier = modifier
                 .fillMaxWidth()
-                .padding(10.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = cardTitle)
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth() ,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = cardTitle)
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth(0.2f)
+                        .height(6.dp) // or .width(64.dp).height(4.dp)
+                        .padding(end = 10.dp),
+                    color = Color.Green,
+                    trackColor = Color.LightGray,
+                    strokeCap = StrokeCap.Round
+                )
+            }
+            Column (
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ){
+                Text(text = "Location")
+                Text(text = "cardTitlecardTitlecardTitlecardTitlecardTitlecardTitlecardTitlecardTitle")
+            }
         }
     }
 }
