18c7217299a2ea79230dca51c7f8138a28624865
Musaib Shabir <bnny633@gmail.com>
2024-07-14 01:42:31 +0530

    Navigation Icons modified, now they are stored as drawables & provided optimization to the NavigationBar

--- app/src/main/java/com/example/foundit/data/NavigationBarData.kt
+++ app/src/main/java/com/example/foundit/data/NavigationBarData.kt
@@ -1,7 +1,5 @@
 package com.example.foundit.data
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
 import com.example.foundit.R
 
 object NavRoutes {
@@ -13,8 +11,8 @@ object NavRoutes {
 
 data class BottomNavigationItem(
     val title: String,
-    val selectedIcon: @Composable () -> Unit,
-    val unselectedIcon: @Composable () -> Unit,
+    val selectedIcon: Int,
+    val unselectedIcon: Int,
     val hasNews: Boolean,
     val badgeCount: Int? = null
 )
@@ -22,27 +20,26 @@ data class BottomNavigationItem(
 val NavigationItems = listOf(
     BottomNavigationItem(
         title = NavRoutes.HOME,
-        selectedIcon = { NavigationIcon(painterResource(id = R.drawable.filled_home)) },
-        unselectedIcon = { NavigationIcon(painterResource(id = R.drawable.outline_home)) },
+        selectedIcon = R.drawable.filled_home,
+        unselectedIcon =  R.drawable.outline_home,
         hasNews = false,
     ),
     BottomNavigationItem(
         title = NavRoutes.PROGRESS,
-        selectedIcon = { NavigationIcon(painterResource(id = R.drawable.outline_progress_activity)) },
-        unselectedIcon ={ NavigationIcon(painterResource(id = R.drawable.outline_progress_activity)) },
-        hasNews = false,
-        badgeCount = 3
+        selectedIcon =  R.drawable.outline_progress_activity,
+        unselectedIcon = R.drawable.outline_progress_activity,
+        hasNews = true,
     ),
     BottomNavigationItem(
         title = NavRoutes.NOTIFICATIONS,
-        selectedIcon = { NavigationIcon(painterResource(id = R.drawable.filled_notifications)) },
-        unselectedIcon = { NavigationIcon(painterResource(id = R.drawable.outline_notifications)) },
+        selectedIcon =  R.drawable.filled_notifications,
+        unselectedIcon =  R.drawable.outline_notifications,
         hasNews = true,
     ),
     BottomNavigationItem(
         title = NavRoutes.PROFILE,
-        selectedIcon = { NavigationIcon(painterResource(id = R.drawable.filled_person)) },
-        unselectedIcon = { NavigationIcon(painterResource(id = R.drawable.outline_profile)) },
+        selectedIcon =  R.drawable.filled_person,
+        unselectedIcon =  R.drawable.outline_profile,
         hasNews = true,
     ),
 )
--- app/src/main/java/com/example/foundit/data/NavigationIcons.kt
+++ app/src/main/java/com/example/foundit/data/NavigationIcons.kt
@@ -2,14 +2,12 @@ package com.example.foundit.data
 
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 
 @Composable
-fun NavigationIcon(painter: Painter)  {
+fun NavigationIcon(iconResource: Int)  {
     Icon(
-        painter = painter,
-        contentDescription = "",
-        tint = Color.Black
+        painter = painterResource(id = iconResource),
+        contentDescription = ""
     )
 }
