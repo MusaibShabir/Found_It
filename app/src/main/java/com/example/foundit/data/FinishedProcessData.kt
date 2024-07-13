2c5c1f0d3ef2d8df10c7e692f24f939dcbc9c55d
Musaib Shabir <bnny633@gmail.com>
2024-07-13 09:55:11 +0530

    Minor formatting modification

--- app/src/main/java/com/example/foundit/data/FinishedProcessData.kt
+++ app/src/main/java/com/example/foundit/data/FinishedProcessData.kt
@@ -8,7 +8,20 @@ data class FinishedProcessDataItem (
     val progressIndicator: Boolean
 )
 val FinishedProcessData = listOf(
-    FinishedProcessDataItem(cardColorCode = 1, itemTitle = "Card 2", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = false),
+    FinishedProcessDataItem(
+        cardColorCode = 1,
+        itemTitle = "Card 2",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = false
+    ),
 
+    FinishedProcessDataItem(
+        cardColorCode = 0,
+        itemTitle = "Ring",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Baramulla Jammu & Kashmir",
+        progressIndicator = false
+    ),
 
     )
--- app/src/main/java/com/example/foundit/data/InProcessData.kt
+++ app/src/main/java/com/example/foundit/data/InProcessData.kt
@@ -9,11 +9,59 @@ data class InProcessDataItem (
 )
 
 val InProcessData = listOf(
-    InProcessDataItem(cardColorCode = 0, itemTitle = "Card 1", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
-    InProcessDataItem(cardColorCode = 1, itemTitle = "Card 2", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
-    InProcessDataItem(cardColorCode = 0, itemTitle = "Card 3", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
-    InProcessDataItem(cardColorCode = 1, itemTitle = "Card 4", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
-    InProcessDataItem(cardColorCode = 0, itemTitle = "Card 5", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
-    InProcessDataItem(cardColorCode = 1, itemTitle = "Card 6", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
-    InProcessDataItem(cardColorCode = 0, itemTitle = "Card 7", itemDescription = "This is the description of the card, which will specify the item and its details.", itemLocation = "Srinagar Jammu & Kashmir", progressIndicator = true),
+    InProcessDataItem(
+        cardColorCode = 0,
+        itemTitle = "Card 1",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
+
+    InProcessDataItem(
+        cardColorCode = 1,
+        itemTitle = "Card 2",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
+
+    InProcessDataItem(
+        cardColorCode = 0,
+        itemTitle = "Card 3",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
+
+    InProcessDataItem(
+        cardColorCode = 1,
+        itemTitle = "Card 4",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
+
+    InProcessDataItem(
+        cardColorCode = 0,
+        itemTitle = "Card 5",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
+
+    InProcessDataItem(
+        cardColorCode = 1,
+        itemTitle = "Card 6",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
+
+    InProcessDataItem(
+        cardColorCode = 0,
+        itemTitle = "Card 7",
+        itemDescription = "This is the description of the card, which will specify the item and its details.",
+        itemLocation = "Srinagar Jammu & Kashmir",
+        progressIndicator = true
+    ),
 )
