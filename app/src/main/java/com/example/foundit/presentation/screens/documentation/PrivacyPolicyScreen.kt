ab53af0626e83b052e9e70f5065d8f2d420a1857
Adnan <adnanafzal01@gmail.com>
2024-08-29 18:20:32 +0530

    Minor font styling in Privacy Policy and Terms of Service screens.

--- app/src/main/java/com/example/foundit/presentation/screens/documentation/PrivacyPolicyScreen.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/documentation/PrivacyPolicyScreen.kt
@@ -18,6 +18,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.foundit.R
 import com.example.foundit.presentation.common.TheTopAppBar
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PrivacyPolicyScreen(
@@ -40,240 +41,240 @@ fun PrivacyPolicyScreen(
                         // Effective Date
             Text(
                 text = stringResource(id = R.string.privacy_policy_date),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Welcome Message
             Text(
                 text = stringResource(id = R.string.privacy_policy_welcome),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Information We Collect
             Text(
                 text = stringResource(id = R.string.privacy_policy_info_collect),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_info_provide),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_account_info),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_user_content),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_auto_collect),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_device_info),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_location_data),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_usage_data),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_third_party),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_social_media),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // How We Use Your Information
             Text(
                 text = stringResource(id = R.string.privacy_policy_use_info),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_provide_improve),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_communicate),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_personalize),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_security_fraud),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // How We Share Your Information
             Text(
                 text = stringResource(id = R.string.privacy_policy_share_info),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_consent),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_service_providers),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_legal_compliance),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_business_transfers),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Your Choices
             Text(
                 text = stringResource(id = R.string.privacy_policy_your_choices),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_access_correction),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_location_tracking),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_opt_out),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Security
             Text(
                 text = stringResource(id = R.string.privacy_policy_security),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_security_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Children's Privacy
             Text(
                 text = stringResource(id = R.string.privacy_policy_children_privacy),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_children_privacy_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // International Users
             Text(
                 text = stringResource(id = R.string.privacy_policy_international_users),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_international_users_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Changes to This Privacy Policy
             Text(
                 text = stringResource(id = R.string.privacy_policy_changes),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_changes_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Contact Us
             Text(
                 text = stringResource(id = R.string.privacy_policy_contact),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_contact_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.privacy_policy_contact_details),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Thank You
             Text(
                 text = stringResource(id = R.string.privacy_policy_thank_you),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
         }
--- app/src/main/java/com/example/foundit/presentation/screens/documentation/TermsofServiceScreen.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/documentation/TermsofServiceScreen.kt
@@ -18,6 +18,8 @@ import androidx.navigation.compose.rememberNavController
 import com.example.foundit.R
 import com.example.foundit.presentation.common.TheTopAppBar
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun TermsOfServiceScreen(
@@ -40,200 +42,200 @@ fun TermsOfServiceScreen(
             // Effective Date
             Text(
                 text = stringResource(id = R.string.terms_of_service_date),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Welcome Message
             Text(
                 text = stringResource(id = R.string.terms_of_service_welcome),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Acceptance of Terms
             Text(
                 text = stringResource(id = R.string.terms_of_service_acceptance),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_acceptance_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Eligibility
             Text(
                 text = stringResource(id = R.string.terms_of_service_eligibility),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_eligibility_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // How Found It Works
             Text(
                 text = stringResource(id = R.string.terms_of_service_how_it_works),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_data_collection),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_matching_system),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_notifications),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // User Responsibilities
             Text(
                 text = stringResource(id = R.string.terms_of_service_user_responsibilities),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_accuracy),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_privacy),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_compliance),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Data Collection and Privacy
             Text(
                 text = stringResource(id = R.string.terms_of_service_data_privacy),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_data_collected),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_data_usage),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_data_security),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Intellectual Property
             Text(
                 text = stringResource(id = R.string.terms_of_service_intellectual_property),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_intellectual_property_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Limitation of Liability
             Text(
                 text = stringResource(id = R.string.terms_of_service_limitation_of_liability),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_limitation_of_liability_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Termination
             Text(
                 text = stringResource(id = R.string.terms_of_service_termination),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_termination_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Changes to the Terms
             Text(
                 text = stringResource(id = R.string.terms_of_service_changes),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_changes_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Governing Law
             Text(
                 text = stringResource(id = R.string.terms_of_service_governing_law),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_governing_law_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Contact Information
             Text(
                 text = stringResource(id = R.string.terms_of_service_contact_information),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.terms_of_service_contact_information_text),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
             // Thank You
             Text(
                 text = stringResource(id = R.string.terms_of_service_thank_you),
-                style = MaterialTheme.typography.bodyLarge,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
                 modifier = Modifier.padding(vertical = 8.dp)
             )
         }
