f3606dc5f2e4824aa5ee6daa3644880166a2610d
Musaib Shabir <bnny633@gmail.com>
2024-09-01 22:21:44 +0530

    Major design modification in DeveloperInfoScreen

--- app/src/main/java/com/example/foundit/presentation/screens/settings/components/clickable/about/DeveloperInfoCard.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/settings/components/clickable/about/DeveloperInfoCard.kt
@@ -2,11 +2,10 @@ package com.example.foundit.presentation.screens.settings.components.clickable.a
 
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,12 +14,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircleOutline
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,11 +29,17 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.foundit.R
+import com.example.foundit.ui.theme.githubColor
+import com.example.foundit.ui.theme.instagramColor
+import com.example.foundit.ui.theme.linkedInColor
+import com.example.foundit.ui.theme.twitterColor
 
 
 @Composable
@@ -45,6 +49,10 @@ fun DeveloperInfoCard(
     firstName: String,
     lastName: String,
     title: String,
+    githubLink: String,
+    linkedInLink: String,
+    twitterLink: String,
+    instagramLink: String,
 ) {
     val urlHandler: UriHandler = LocalUriHandler.current
 
@@ -52,7 +60,7 @@ fun DeveloperInfoCard(
         shape = RoundedCornerShape(15.dp),
         modifier = modifier
             .fillMaxWidth()
-            .height(180.dp)
+            .height(220.dp)
             .padding(16.dp),
     ) {
         Row(
@@ -63,7 +71,7 @@ fun DeveloperInfoCard(
                 modifier = modifier
                     .fillMaxSize()
                     .weight(.4f)
-                    .padding(16.dp),
+                    .padding(1.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
@@ -71,125 +79,181 @@ fun DeveloperInfoCard(
                     painter = profilePicture,
                     contentDescription = "Developer's profile picture ",
                     alignment = Alignment.Center,
-                    modifier = Modifier.clip(shape = CircleShape)
+                    modifier = Modifier
+                        .clip(shape = CircleShape)
+                        .size(130.dp),
+
                 )
             }
 
             Column(
                 modifier = modifier
                     .fillMaxSize()
-                    .weight(.6f)
-                    .padding(top = 5.dp)
-                    .padding(16.dp),
+                    .weight(.6f),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top
             ) {
-                Row(
+                Column(
                     modifier = modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "$firstName $lastName",
-                        style = MaterialTheme.typography.bodyLarge,
-                        fontWeight = FontWeight(600),
-                        color = Color.Black,
-                        maxLines = 1
-                    )
-                }
-                Row(
-                    modifier = modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleSmall,
-                        fontWeight = FontWeight(200),
-                        color = Color.Black,
-                        maxLines = 1
-                    )
-                }
-                Spacer(modifier = modifier.height(10.dp))
-                Row(
-                    modifier = modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Surface(
-                        shape = RoundedCornerShape(15.dp),
-                        color = Color.LightGray,
-                        modifier = Modifier
-                            .height(20.dp)
-                            .width(60.dp)
+                        .fillMaxWidth()
+                        .padding(top = 28.dp, bottom = 5.dp)
+                ){
+                    //Name & Title Column
+                    Column(
+                        modifier = modifier,
+                        verticalArrangement = Arrangement.SpaceBetween
+
                     ) {
                         Row(
-                            modifier = modifier,
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
+                            modifier = modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Icon(
-                                imageVector = Icons.Filled.AddCircleOutline,
-                                contentDescription = "GitHub",
-                                tint = Color.Black,
-                                modifier = Modifier.size(10.dp)
-
-
+                            Text(
+                                text = "$firstName $lastName",
+                                style = MaterialTheme.typography.bodyLarge,
+                                fontWeight = FontWeight(900),
+                                fontSize = 18.sp,
+                                color = Color.Black,
+                                maxLines = 1
                             )
-                            Spacer(modifier = modifier.width(3.dp))
+                        }
+                        Row(
+                            modifier = modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = title,
+                                style = MaterialTheme.typography.titleSmall,
+                                fontWeight = FontWeight(300),
+                                color = Color.Black,
+                                fontStyle = FontStyle.Italic,
+                                fontSize = 14.sp,
+                                maxLines = 1
+                            )
+                        }
+                    }
 
+                }
+                HorizontalDivider(
+                    modifier = modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(bottom = 5.dp),
+                    thickness = 1.dp,
+                    color = Color.Black
+                )
+
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        ElevatedButton(
+                            modifier = modifier.width(100.dp),
+                            onClick = {urlHandler.openUri(githubLink)},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = githubColor,
+                                contentColor = Color.White,
+                            ),
+                            elevation = ButtonDefaults.buttonElevation(
+                                defaultElevation = 24.dp
+                            )
+
+                        ) {
                             Text(
                                 text = "GitHub",
-                                color = Color.Black,
-                                fontSize = 10.sp,
+                                color = Color.White,
+                                fontSize = 12.sp,
                                 fontWeight = FontWeight.Medium,
-                                modifier = modifier.clickable {
-                                    urlHandler.openUri("https://github.com/MusaibShabir")
-                                }
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
+                        ElevatedButton(
+                            modifier = modifier.width(100.dp),
+                            onClick = {urlHandler.openUri(linkedInLink)},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = linkedInColor,
+                                contentColor = Color.White,
+                            ),
+                            elevation = ButtonDefaults.buttonElevation(
+                                defaultElevation = 24.dp
+                            )
+
+                        ) {
+                            Text(
+                                text = "LinkedIn",
+                                color = Color.White,
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Medium,
+                                textAlign = TextAlign.Center
                             )
                         }
                     }
 
-                    Surface(
-                        shape = RoundedCornerShape(15.dp),
-                        color = Color.LightGray,
-                        modifier = Modifier
-                            .height(20.dp)
-                            .width(60.dp)
+
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Row(
-                            modifier = modifier,
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center,
 
-                            ) {
-                            Icon(
-                                imageVector = Icons.Filled.AddCircleOutline,
-                                contentDescription = "LinkedIn",
-                                tint = Color.Black,
-                                modifier = Modifier.size(10.dp)
-
+                        ElevatedButton(
+                            modifier = modifier.width(100.dp),
+                            onClick = {urlHandler.openUri(twitterLink)},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = twitterColor,
+                                contentColor = Color.White,
+                            ),
+                            elevation = ButtonDefaults.buttonElevation(
+                                defaultElevation = 24.dp
                             )
-                            Spacer(modifier = modifier.width(3.dp))
 
+                        ) {
                             Text(
-                                text = "LinkedIn",
-                                color = Color.Black,
-                                fontSize = 10.sp,
+                                text = "Twitter",
+                                color = Color.White,
+                                fontSize = 12.sp,
                                 fontWeight = FontWeight.Medium,
-                                modifier = modifier
-                                    .clickable {
-                                        urlHandler.openUri(
-                                            "https://www.linkedin.com/in/musaib-shabir-49179b244/"
-                                        )
-                                    }
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
+                        ElevatedButton(
+                            modifier = modifier.width(100.dp),
+                            onClick = {urlHandler.openUri(instagramLink)},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = instagramColor,
+                                contentColor = Color.White,
+                            ),
+                            elevation = ButtonDefaults.buttonElevation(
+                                defaultElevation = 24.dp
+                            )
+
+                        ) {
+                            Text(
+                                text = "Instagram",
+                                color = Color.White,
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Medium,
+                                textAlign = TextAlign.Center
                             )
                         }
                     }
                 }
+
             }
 
         }
@@ -198,6 +262,7 @@ fun DeveloperInfoCard(
     }
 
 }
+
 
 @Preview(showBackground = true, showSystemUi = false)
 @Composable
@@ -208,9 +273,10 @@ fun PreviewDeveloperInfo() {
         profilePicture = profilePicture,
         firstName = "Musaib",
         lastName = "Shabir Dar",
-        title = "Android Developer"
+        title = "Android Developer",
+        githubLink = "",
+        linkedInLink = "",
+        twitterLink = "",
+        instagramLink = ""
     )
 }
-
-
-
--- app/src/main/java/com/example/foundit/presentation/screens/settings/components/clickable/about/DeveloperInfoScreen.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/settings/components/clickable/about/DeveloperInfoScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.foundit.R
 import com.example.foundit.presentation.common.TheTopAppBar
@@ -29,13 +28,54 @@ fun DeveloperInfoScreen(
             modifier = modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(horizontal = 16.dp)
         ) {
+
+            //Musaib Shabir
+            DeveloperInfoCard(
+                profilePicture = painterResource(id = R.drawable.musaib_shabir_dp),
+                firstName = "Musaib",
+                lastName = "Shabir",
+                title = "Team Lead & Senior Developer",
+                githubLink = "https://github.com/musaibshabir",
+                linkedInLink = "https://www.linkedin.com/in/musaib-shabir-dar/",
+                twitterLink = "https://twitter.com/musaib_shabir",
+                instagramLink = "https://www.instagram.com/musaib.shabir/"
+            )
+
+            //Qazi Huzaif
             DeveloperInfoCard(
                 profilePicture = painterResource(id = R.drawable.ic_launcher_background),
+                firstName = "Qazi Muhammad",
+                lastName = "Huzaif",
+                title = "Contributor",
+                githubLink = "https://github.com/musaibshabir",
+                linkedInLink = "https://www.linkedin.com/in/musaib-shabir-dar/",
+                twitterLink = "https://twitter.com/musaib_shabir",
+                instagramLink = "https://www.instagram.com/musaib.shabir/"
+            )
+
+            //Adnan Afzal
+            DeveloperInfoCard(
+                profilePicture = painterResource(id = R.drawable.ic_launcher_background),
+                firstName = "Adnan",
+                lastName = "Afzal",
+                title = "Contributor",
+                githubLink = "https://github.com/musaibshabir",
+                linkedInLink = "https://www.linkedin.com/in/musaib-shabir-dar/",
+                twitterLink = "https://twitter.com/musaib_shabir",
+                instagramLink = "https://www.instagram.com/musaib.shabir/"
+            )
+
+            //Sofi Burhan
+            DeveloperInfoCard(
+                profilePicture = painterResource(id = R.drawable.profile_picture),
                 firstName = "Sofi",
-                lastName = "Burhon",
-                title = "UI-UX Developer"
+                lastName = "Burhan",
+                title = "Contributor",
+                githubLink = "https://github.com/musaibshabir",
+                linkedInLink = "https://www.linkedin.com/in/musaib-shabir-dar/",
+                twitterLink = "https://twitter.com/musaib_shabir",
+                instagramLink = "https://www.instagram.com/musaib.shabir/"
             )
         }
     }
