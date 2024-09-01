d226704d724fd31381316be6e13006ebe90ae845
huzaif1234 <huzaifqazi25@gmail.com>
2024-09-01 15:23:39 +0530

    addItemData testing

--- app/src/main/java/com/example/foundit/presentation/screens/actions/ActionComponentViewModel.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/actions/ActionComponentViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.foundit.presentation.screens.actions
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.foundit.presentation.data.firestore.FirestoreService
@@ -14,7 +15,11 @@ class ActionComponentViewModel @Inject constructor(
     fun sendData(phone : String,model : String,color : String, onResult: (Boolean) -> Unit){
         viewModelScope.launch {
             try {
-                firestoreService.addItemData(phone, model, color)
+                try {
+                    firestoreService.addItemData(phone, model, color)
+                } catch (e: Exception) {
+                    Log.d("addItem", "sendData: e")
+                }
                 firestoreService.getItemData()
                 onResult(true)
             } catch (e: Exception) {
