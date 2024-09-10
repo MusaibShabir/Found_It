c71a9f8d52a0003cadfe9dd964b8a1f2d5dce7ab
Musaib Shabir <bnny633@gmail.com>
2024-09-10 21:29:51 +0530

    deleted flags folder

--- app/src/main/java/com/example/foundit/presentation/screens/home/components/AppName.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/home/components/AppName.kt
@@ -1,14 +1,13 @@
 package com.example.foundit.presentation.screens.home.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,10 +25,9 @@ fun AppIcon(
         verticalAlignment = Alignment.CenterVertically
     )
     {
-        Icon(
+        Image(
             painter = painterResource(R.drawable.app_icon_home),
-            contentDescription = "",
-            tint = Color.Unspecified,
+            contentDescription = "App Logo",
             modifier = modifier
         )
     }
