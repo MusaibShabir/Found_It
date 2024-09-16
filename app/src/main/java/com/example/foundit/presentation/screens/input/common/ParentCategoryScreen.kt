3677b9deee52cb976817ec2dac9e5b8a2de86016
Musaib Shabir <bnny633@gmail.com>
2024-09-17 01:02:31 +0530

    major design modification

--- app/src/main/java/com/example/foundit/presentation/screens/input/common/ParentCategoryScreen.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/input/common/ParentCategoryScreen.kt
@@ -1,27 +1,36 @@
 package com.example.foundit.presentation.screens.input.common
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.foundit.presentation.common.TheTopAppBar
 import com.example.foundit.presentation.screens.input.data.parentCategories
 
 @Composable
-fun ParentCategoryScreen(modifier: Modifier = Modifier) {
-    Column(
+fun ParentCategoryScreen(
+    modifier: Modifier,
+    navController: NavController
+) {
+    Scaffold(
         modifier = modifier.fillMaxSize(),
-    ){
+        topBar = { TheTopAppBar(title = "Report Lost Item", navController = navController) },
+        bottomBar = { InputBottomNavigationBar(modifier = modifier,  onCancelClick = { /*TODO*/ } , onNextClick = { /*TODO*/ }) }
+
+    ){ paddingValues ->
         LazyVerticalGrid(
             modifier = modifier
                 .fillMaxSize()
-                .padding(bottom = 24.dp),
+                .padding(paddingValues),
             verticalArrangement = Arrangement.Bottom,
             columns = GridCells.Adaptive(minSize = 110.dp)
         ) {
@@ -37,9 +46,13 @@ fun ParentCategoryScreen(modifier: Modifier = Modifier) {
 }
 
 
+
 @Preview(showBackground = true, showSystemUi = true, device = "id:pixel_2")
 @Composable
 fun PreviewParentCategoryScreen() {
-    ParentCategoryScreen()
+    ParentCategoryScreen(
+        modifier = Modifier,
+        navController = NavController(LocalContext.current)
+    )
 }
 
