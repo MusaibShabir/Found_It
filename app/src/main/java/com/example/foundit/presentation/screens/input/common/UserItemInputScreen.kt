9ba0660deb591c547bfc6a2af1dfd0ba61e21d66
Musaib Shabir <bnny633@gmail.com>
2024-09-17 18:33:22 +0530

    alert dialog added

--- app/src/main/java/com/example/foundit/presentation/screens/input/common/UserItemInputScreen.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/input/common/UserItemInputScreen.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,15 +20,23 @@ import androidx.navigation.NavController
 import com.example.foundit.presentation.common.TheTopAppBar
 import com.example.foundit.presentation.screens.input.data.parentCategories
 
+
 @Composable
 fun UserItemInputScreen(
     modifier: Modifier,
     navController: NavController
 ) {
+    var showAlertDialogBox by remember { mutableStateOf(false) } // State for the alert dialog
+
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = { TheTopAppBar(title = "Report Lost Item", navController = navController) },
-        bottomBar = { InputBottomNavigationBar(modifier = modifier,  onCancelClick = { /*TODO*/ } , onNextClick = { /*TODO*/ }) }
+        bottomBar = {
+            InputBottomNavigationBar(
+                modifier = modifier,
+                onCancelOrBackClick = { showAlertDialogBox = true },
+                onNextClick = { /*TODO*/ })
+        }
 
     ){ paddingValues ->
         LazyVerticalGrid(
@@ -42,7 +54,28 @@ fun UserItemInputScreen(
                 )
             }
         }
+
     }
+
+    if (showAlertDialogBox) {
+        AreYouSureToCancelAlertBox(
+            modifier = modifier,
+            onDismissRequest = { showAlertDialogBox = false },
+            navController = navController
+        )
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun PreviewAreYouSureToCancelAlertBox() {
+    AreYouSureToCancelAlertBox(
+        modifier = Modifier,
+        onDismissRequest = {},
+        navController = NavController(LocalContext.current)
+    )
 }
 
 
