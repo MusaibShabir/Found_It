afbfe18f525f6684442466eac450fbf090872abb
Musaib Shabir <bnny633@gmail.com>
2024-09-21 05:59:36 +0530

    some minor modification

--- app/src/main/java/com/example/foundit/presentation/screens/registration/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/registration/signup/SignUpScreen.kt
@@ -1,13 +1,20 @@
 package com.example.foundit.presentation.screens.registration.signup
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.TextSelectionColors
@@ -20,14 +27,31 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Man
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.input.*
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
@@ -38,12 +62,8 @@ import com.example.foundit.presentation.screens.registration.components.Clickabl
 import com.example.foundit.presentation.screens.registration.components.OrDivider
 import com.example.foundit.presentation.screens.registration.components.google.ContinueWithGoogleCard
 import com.example.foundit.presentation.screens.registration.components.google.ContinueWithGoogleViewModel
-import com.example.foundit.presentation.screens.registration.login.getLastLocation
-import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -76,7 +96,7 @@ fun SignUpScreen(
         if (isGranted) {
             Log.d("Location", "Permission granted")
             coroutineScope.launch {
-                getLastLocation(fusedLocationClient, context)
+                signUpViewModel.getLastLocation(fusedLocationClient, context)
             }
         } else {
             Toast.makeText(context, "Location permission is required to proceed", Toast.LENGTH_LONG)
@@ -92,7 +112,7 @@ fun SignUpScreen(
             ) == PackageManager.PERMISSION_GRANTED -> {
                 Log.d("Location", "Permission already granted")
                 coroutineScope.launch {
-                    getLastLocation(fusedLocationClient, context)
+                    signUpViewModel.getLastLocation(fusedLocationClient, context)
                 }
             }
 
@@ -411,33 +431,7 @@ fun SignUpScreen(
         }
     }
 
-    suspend fun getLastLocation(
-        fusedLocationClient: FusedLocationProviderClient,
-        context: Context
-    ) {
-        if (ContextCompat.checkSelfPermission(
-                context, Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            try {
-                val location = fusedLocationClient.lastLocation.await() // Use await() to make it suspendable
-                location?.let {
-                    val userLocation = LatLng(it.latitude, it.longitude)
-                    Log.d("Location", "User is at: $userLocation")
-                } ?: run {
-                    Toast.makeText(context, "Location not available, make sure GPS is enabled", Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: SecurityException) {
-                e.printStackTrace()
-                Toast.makeText(context, "Location permission is required to access location", Toast.LENGTH_SHORT).show()
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(context, "Failed to get location", Toast.LENGTH_SHORT).show()
-            }
-        } else {
-            Toast.makeText(context, "Location permission is not granted", Toast.LENGTH_SHORT).show()
-        }
-    }
+
 
 }
 
