70245655e60bd23dbd86e7f183b0ce5be1632d07
Musaib Shabir <bnny633@gmail.com>
2024-09-26 01:29:27 +0530

    major ui modification

--- app/src/main/java/com/example/foundit/presentation/screens/input/common/components/AreYouSureAlertDialog.kt
+++ app/src/main/java/com/example/foundit/presentation/screens/input/common/components/AreYouSureAlertDialog.kt
@@ -1,9 +1,11 @@
 package com.example.foundit.presentation.screens.input.common.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.foundit.presentation.data.navigation.NavRoutes
+import com.example.foundit.ui.theme.MainGreen
 import com.example.foundit.ui.theme.RobotFamily
 
 @Composable
@@ -24,16 +27,21 @@ fun AreYouSureToCancelAlertBox(
     navController: NavController
 ) {
     AlertDialog(
-        modifier = modifier,
+        modifier = modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colorScheme.background,
         onDismissRequest = onDismissRequest,
         title = { Text(
-            text = "Are you sure you want to cancel?",
+            text = "Are you Sure?",
             textAlign = TextAlign.Start,
             fontSize = 22.sp,
             fontWeight = FontWeight.Medium,
             fontStyle = FontStyle.Normal,
             fontFamily = RobotFamily
         ) },
+        text = { Text(
+            text = "This action cannot be undone.",
+            fontSize = 16.sp
+        )},
         confirmButton = {
             Button(
                 colors = ButtonDefaults.buttonColors(containerColor = Color.Red.copy(alpha = 0.7f)),
@@ -47,6 +55,7 @@ fun AreYouSureToCancelAlertBox(
         dismissButton = {
             Button(
                 onClick = onDismissRequest,
+                colors = ButtonDefaults.buttonColors(containerColor = MainGreen),
                 modifier = modifier.padding(horizontal = 10.dp)
             ) {
                 Text("No")
