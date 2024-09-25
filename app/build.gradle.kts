a5a78b548172beb80ce5d316fb99550d5b20f6d6
Musaib Shabir <bnny633@gmail.com>
2024-09-26 02:48:05 +0530

    redundant lib removed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,7 +137,6 @@ dependencies {
     implementation (libs.okhttp)
 
     // For parsing JSON (Gson or kotlinx.serialization)
-    implementation (libs.gson)
     implementation (libs.kotlinx.serialization.json)
 
 
